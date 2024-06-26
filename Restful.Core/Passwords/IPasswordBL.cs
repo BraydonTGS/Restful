df927568925e94a8115575dd2b15c9d6218c97bf
BraydonTGS <braydonsutherland@gmail.com>
2024-06-26 16:34:47 -0500

    Created PasswordBL and IPasswordBL

---
+++ Restful.Core/Passwords/IPasswordBL.cs
@@ -0,0 +1,11 @@
+﻿using Restful.Core.Passwords.Model;
+using Restful.Global.Enums;
+
+namespace Restful.Core.Passwords
+{
+    public interface IPasswordBL
+    {
+        Task<Password?> CreatePasswordForUserAsync(Guid userId, string password);
+        Task<PasswordVerificationResults> VerifyUserPasswordAsync(Guid userId, string providedPassword);
+    }
+}
