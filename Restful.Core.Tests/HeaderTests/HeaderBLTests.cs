f68880d1c7e3678801e3b508a672e71d2a1d2ee6
BraydonTGS <braydonsutherland@gmail.com>
2024-07-11 09:55:55 -0500

    Huge Database Seeder Refactor and Quick Implementation of the HeaderBLTests

---
+++ Restful.Core.Tests/HeaderTests/HeaderBLTests.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prism.Ioc;
+using Restful.Core.Context;
+using Restful.Core.Headers;
+using Restful.Core.Headers.Models;
+using Restful.Tests.Shared.Base;
+using Restful.Tests.Shared.Database;
+
+namespace Restful.Core.Tests.HeaderTests
+{
+    [TestClass]
+    public class HeaderBLTests : TestBase
+    {
+        private readonly IHeaderBL _headerBL;
+        private readonly IDbContextFactory<RestfulDbContext> _dbContextFactory;
+        public HeaderBLTests()
+        {
+            _headerBL = _containerProvider.Resolve<IHeaderBL>();
+            _dbContextFactory = _containerProvider.Resolve<IDbContextFactory<RestfulDbContext>>();
+        }
+
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await DatabaseSeeder.Seed(_dbContextFactory);
+        }
+
+        [TestMethod]
+        public void ConstructorNotNull_Success()
+        {
+            Assert.IsNotNull(_headerBL);
+        }
+
+        [TestMethod]
+        public async Task GetAllHeadersAsync_Success()
+        {
+            var results = await _headerBL.GetAllAsync();
+
+            Assert.AreEqual(3, results.Count());
+        }
+
+
+        [TestMethod]
+        public async Task GetHeaderByIdAsync_Success()
+        {
+            var header = await _headerBL.GetByIdAsync(DatabaseSeeder.GetHeaderId());
+
+            Assert.IsNotNull(header);
+
+        }
+
+        [TestMethod]
+        public async Task CreateHeaderAsync_Success()
+        {
+            var header = new Header()
+            {
+                Key = "Content-Type",
+                Value = "application/json", 
+                Enabled = true,
+                RequestId = DatabaseSeeder.GetRequestId()
+            };
+
+            header = await _headerBL.CreateAsync(header);
+
+            Assert.IsNotNull(header);
+        }
+
+        [TestMethod]
+        public async Task UpdateToDoItemAsync_Success()
+        {
+            var header = new Header()
+            {
+                Key = "Content-Type",
+                Value = "application/json",
+                Enabled = true,
+                RequestId = DatabaseSeeder.GetRequestId()
+            };
+
+            header = await _headerBL.CreateAsync(header);
+
+            Assert.IsNotNull(header);
+
+            header = await _headerBL.UpdateAsync(header);
+
+            Assert.IsNotNull(header);
+        }
+
+        [TestMethod]
+        public async Task SoftDeleteToDoItemAsync_Success()
+        {
+            var header = new Header()
+            {
+                Key = "Content-Type",
+                Value = "application/json",
+                Enabled = true,
+                RequestId = DatabaseSeeder.GetRequestId()
+            };
+
+            header = await _headerBL.CreateAsync(header);
+
+            Assert.IsNotNull(header);
+
+            var allHeaders = await _headerBL.GetAllAsync();
+
+            Assert.AreEqual(4, allHeaders.Count());
+
+            var results = await _headerBL.SoftDeleteAsync(header.Id);
+
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results);
+
+            allHeaders = await _headerBL.GetAllAsync();
+
+            Assert.AreEqual(3, allHeaders.Count());
+        }
+
+        [TestMethod]
+        public async Task HardDeleteToDoItemAsync_Success()
+        {
+            var header = new Header()
+            {
+                Key = "Content-Type",
+                Value = "application/json",
+                Enabled = true,
+                RequestId = DatabaseSeeder.GetRequestId()
+            };
+
+            header = await _headerBL.CreateAsync(header);
+
+            Assert.IsNotNull(header);
+
+            var results = await _headerBL.HardDeleteAsync(header.Id);
+
+            Assert.IsTrue(results);
+        }
+
+        [TestMethod]
+        public async Task RestoreToDoItemAsync_Success()
+        {
+            var header = new Header()
+            {
+                Key = "Content-Type",
+                Value = "application/json",
+                Enabled = true,
+                RequestId = DatabaseSeeder.GetRequestId()
+            };
+
+            header = await _headerBL.CreateAsync(header);
+
+            Assert.IsNotNull(header);
+
+            await _headerBL.SoftDeleteAsync(header.Id);
+
+            var allHeaders = await _headerBL.GetAllAsync();
+
+            Assert.AreEqual(3, allHeaders.Count());
+
+            var results = await _headerBL.RestoreAsync(header.Id);
+
+            Assert.IsTrue(results);
+
+            allHeaders = await _headerBL.GetAllAsync();
+
+            Assert.AreEqual(4, allHeaders.Count());
+        }
+
+        [TestCleanup]
+        public async Task TestCleanup()
+        {
+            await DatabaseSeeder.Clear(_dbContextFactory);
+        }
+    }
+}
--- Restful.Tests.Shared/Database/DatabaseSeeder.cs
+++ Restful.Tests.Shared/Database/DatabaseSeeder.cs
@@ -12,16 +12,102 @@ namespace Restful.Tests.Shared.Database
         private static Guid _userId;
         private static Guid _secondUserId;
         private static Guid _requestId;
-        private static Guid _collectionGuid;
+        private static Guid _collectionId;
+        private static Guid _headerId;
+
+        public static Guid GetUserId() => _userId;
+        public static Guid GetSecondUserId() => _secondUserId;
+        public static Guid GetRequestId() => _requestId;
+        public static Guid GetCollectionId() => _collectionId;
+        public static Guid GetHeaderId() => _headerId;
 
         public static async Task Seed(
             IDbContextFactory<RestfulDbContext> contextFactory)
         {
-            IPasswordHasher<Password> passwordHasher = new PasswordHasher();
             using var context = await contextFactory.CreateDbContextAsync();
 
             // User //
-            var user = new UserEntity()
+            UserEntity user, secondUser;
+            GenerateUserEntities(out user, out secondUser);
+
+            await context.Users.AddAsync(user);
+            await context.Users.AddAsync(secondUser);
+
+            // Collection //
+            CollectionEntity collection;
+            GenerateCollectionEntity(user.Id, out collection);
+
+            await context.AddAsync(collection);
+
+            // Requests //
+            RequestEntity requestOne, requestTwo;
+            GenerateRequestEntities(user.Id, collection.Id, out requestOne, out requestTwo);
+
+            await context.AddAsync(requestOne);
+            await context.AddAsync(requestTwo);
+
+            // Headers //
+            HeaderEntity headerOne, headerTwo, headerThree;
+            GenerateHeaderEntities(requestOne.Id, out headerOne, out headerTwo, out headerThree);
+
+            await context.AddAsync(headerOne);
+            await context.AddAsync(headerTwo);
+            await context.AddAsync(headerThree);
+
+            // Password //
+            PasswordEntity password;
+            GeneratePasswordEntity(user.Id, out password);
+
+            await context.AddAsync(password);
+
+            // Set Ids //
+            _userId = user.Id;
+            _secondUserId = secondUser.Id;
+            _requestId = requestOne.Id;
+            _collectionId = collection.Id;
+            _headerId = headerOne.Id;
+
+            // Save Changes //
+            await context.SaveChangesAsync();
+        }
+
+        #region Clear
+        /// <summary>
+        /// Clear the Database
+        /// </summary>
+        /// <param name="contextFactory"></param>
+        /// <returns></returns>
+        public static async Task Clear(IDbContextFactory<RestfulDbContext> contextFactory)
+        {
+            using var context = await contextFactory.CreateDbContextAsync();
+
+            context.Users.RemoveRange();
+            context.Collections.RemoveRange();
+            context.Responses.RemoveRange();
+            context.Headers.RemoveRange();
+
+            await context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Generate Entities For Seeding
+        private static void GenerateCollectionEntity(
+            Guid userId,
+            out CollectionEntity entity)
+        {
+            entity = new CollectionEntity()
+            {
+                Title = "LOTR API",
+                Description = "A Restful Collection for testing the LOTR API",
+                UserId = userId
+            };
+        }
+
+        private static void GenerateUserEntities(
+            out UserEntity user,
+            out UserEntity secondUser)
+        {
+            user = new UserEntity()
             {
                 FirstName = "Daniel",
                 LastName = "Aguirre",
@@ -30,8 +116,7 @@ namespace Restful.Tests.Shared.Database
                 Description = "JarJar of Unit Tests",
 
             };
-
-            var secondUser = new UserEntity()
+            secondUser = new UserEntity()
             {
                 FirstName = "Braydon",
                 LastName = "Sutherland",
@@ -39,71 +124,71 @@ namespace Restful.Tests.Shared.Database
                 UserName = "Geo",
                 Description = "App Admin",
             };
+        }
 
-            await context.Users.AddAsync(user);
-            await context.Users.AddAsync(secondUser);
-
-            var collection = new CollectionEntity()
-            {
-                Title = "LOTR API",
-                Description = "A Restful Collection for testing the LOTR API",
-                UserId = user.Id
-            };
-
-            await context.AddAsync(collection);
-
-            var requestOne = new RequestEntity()
+        static void GenerateRequestEntities(
+            Guid userId,
+            Guid collectionId,
+            out RequestEntity requestOne,
+            out RequestEntity requestTwo)
+        {
+            requestOne = new RequestEntity()
             {
                 Name = "GetAllCharacters",
                 Description = "Get All LOTR Characters",
                 HttpMethod = Global.Enums.HttpMethod.GET,
                 Url = @"https://the-one-api.dev/v2/characters",
-                CollectionId = collection.Id
+                CollectionId = collectionId
             };
-
-            var requestTwo = new RequestEntity()
+            requestTwo = new RequestEntity()
             {
                 Name = "GetAllBooks",
                 Description = "Get All LOTR Books",
                 HttpMethod = Global.Enums.HttpMethod.GET,
                 Url = @"https://the-one-api.dev/v2/books",
-                UserId = user.Id
+                UserId = userId
             };
+        }
 
+        private static void GenerateHeaderEntities(
+            Guid requestId,
+            out HeaderEntity headerOne,
+            out HeaderEntity headerTwo,
+            out HeaderEntity headerThree)
+        {
+            headerOne = new HeaderEntity()
+            {
+                Key = "Accept",
+                Value = "application/json",
+                Enabled = true,
+                RequestId = requestId
+            };
+            headerTwo = new HeaderEntity()
+            {
+                Key = "Content-Type",
+                Value = "*/*",
+                Enabled = true,
+                RequestId = requestId
+            };
+            headerThree = new HeaderEntity()
+            {
+                Key = "Connection",
+                Value = "keep-alive",
+                Enabled = true,
+                RequestId = requestId
+            };
+        }
 
-            await context.AddAsync(requestOne);
-            await context.AddAsync(requestTwo);
+        private static void GeneratePasswordEntity(
+            Guid userId,
+            out PasswordEntity password)
+        {
+            IPasswordHasher<Password> passwordHasher = new PasswordHasher();
 
             var (hash, salt) = passwordHasher.HashPassword("YodaIsMyMentor");
 
-            // Password //
-            var password = new PasswordEntity(user.Id, hash, salt);
-
-            await context.AddAsync(password);
-
-            _userId = user.Id;
-            _secondUserId = secondUser.Id;
-            _requestId = requestOne.Id;
-            _collectionGuid = collection.Id;
-
-            await context.SaveChangesAsync();
+            password = new PasswordEntity(userId, hash, salt);
         }
-
-        public static async Task Clear(IDbContextFactory<RestfulDbContext> contextFactory)
-        {
-            using var context = await contextFactory.CreateDbContextAsync();
-
-            context.Users.RemoveRange();
-            context.Collections.RemoveRange();
-            context.Responses.RemoveRange();
-
-            await context.SaveChangesAsync();
-        }
-
-        public static Guid GetUserId() => _userId;
-
-        public static Guid GetSecondUserId() => _secondUserId;
-        public static Guid GetRequestId() => _requestId;
-        public static Guid GetCollectionId() => _collectionGuid;
+        #endregion
     }
 }
